d5843524cbdec4ef47f3056e9296e08d490e66bd
Capriceulia <95130454+Capriceulia@users.noreply.github.com>
2023-10-08 23:11:09 +0500

    Самостоятельная_работа_2.py

---
+++ Самостоятельная_работа_2.py
@@ -0,0 +1,16 @@
+```python
+def read_file (f_name):
+    try:
+        with open(f_name, "r") as file:
+            f = file.read()
+            if not f:
+                raise Exception("Файл пустой")
+            else:
+                print(f)
+    except FileNotFoundError:
+        print("Файл не найден")
+    except Exception as ex:
+         print(ex)
+f_name = "file.txt"
+read_file(f_name)
+```

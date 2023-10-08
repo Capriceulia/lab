303a995dcb1fe744881997b57592bb39a6e85762
Capriceulia <95130454+Capriceulia@users.noreply.github.com>
2023-10-08 21:18:54 +0500

    Самостоятельная_работа_3.py

---
+++ Самостоятельная_работа_3.py
@@ -0,0 +1,9 @@
+```python
+from datetime import datetime
+import datetime
+start_time = datetime.now()
+for i in range (5):
+    current_time = datetime.now()
+    print(f"Текущее время {current_time.strftime('%H:%M:%S')}")
+    time.sleep(1)
+```

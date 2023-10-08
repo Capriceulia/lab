64d918f2c767621fbcd210fcb200f4240dfdf853
Capriceulia <95130454+Capriceulia@users.noreply.github.com>
2023-10-08 22:51:47 +0500

    Самостоятельная_работа_1.py

---
+++ Самостоятельная_работа_1.py
@@ -0,0 +1,9 @@
+```python
+class Pet:
+    def __init__(self, type, breed, name):
+        self.type = type
+        self.breed = breed
+        self.name = name
+        print(f"{self.type} {self.breed} {self.name}")
+my_pet = Pet("Cat","Sphinx","Glasha")
+```

3f4f79c88db2f8af6101d517ab26f5b0fafc5554
Capriceulia <95130454+Capriceulia@users.noreply.github.com>
2023-10-08 23:30:49 +0500

    Самостоятельная_работа_1.py

---
+++ Самостоятельная_работа_1.py
@@ -0,0 +1,14 @@
+```python
+def fib():
+    a, b = 1, 1  
+    count = 0  
+    while count < 10:  
+        if a >= 200:
+            yield a  
+            count += 1 
+        a, b = b, a + b  
+
+fibonacci= fib()
+for number in fibonacci:
+    print(number)
+```
